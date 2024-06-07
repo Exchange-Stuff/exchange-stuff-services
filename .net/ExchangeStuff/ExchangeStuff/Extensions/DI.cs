29745d6946db079b9ae596226435eae43809ba64
nguynminhvu <nguyenminhvu.august@gmail.com>
2024-06-07 11:38:30 +0700

    auth p2

---
+++ .net/ExchangeStuff/ExchangeStuff/Extensions/DI.cs
@@ -0,0 +1,14 @@
+﻿using ExchangeStuff.AuthOptions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ExchangeStuff.Extensions
+{
+    public static class DI
+    {
+        public static void InjectAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IAuthorizationPolicyProvider, ESPolicy>();
+            services.AddTransient<IAuthorizationHandler, ActionRequirementHandler>();
+        }
+    }
+}
