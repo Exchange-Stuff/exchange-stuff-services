ee834dc73cc64e32b5a20f052a4538b4e89cee4b
khanhncd2008 <rinright1306@gmail.com>
2024-06-07 06:11:05 +0100

    khanhncd

---
+++ .net/ExchangeStuff/ExchangeStuff.Service/Services/Impls/PurchaseTicketService.cs
@@ -0,0 +1,15 @@
+﻿using ExchangeStuff.CurrentUser.Users;
+using ExchangeStuff.Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeStuff.Service.Services.Impls
+{
+    public class PurchaseTicketService :IPurchaseTicketService
+    {
+        private readonly IIdentityUser<Guid> _identityUser;
+    }
+}
