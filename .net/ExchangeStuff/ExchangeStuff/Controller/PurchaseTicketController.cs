f6f99b54329ba963aeb9b20dd11cb202cbf32bb8
khanhncd2008 <rinright1306@gmail.com>
2024-06-09 16:41:38 +0700

    TransactionHistoryController

--- .net/ExchangeStuff/ExchangeStuff/Controller/PurchaseTicketController.cs
+++ .net/ExchangeStuff/ExchangeStuff/Controller/PurchaseTicketController.cs
@@ -16,9 +16,23 @@ namespace ExchangeStuff.Controller
         }
 
         [HttpGet("/getAllPurchaseTicket/{pageSize}/{pageIndex}/{status}")]
-        public async Task<ActionResult> Get(int pageSize, int pageIndex, PurchaseTicketStatus status)
+        public async Task<IActionResult> GetAllPurchaseTicket(int pageSize, int pageIndex, PurchaseTicketStatus status)
         {
             var listPurchaseTicket = await _purchaseTicketService.GetAllPurchaseTicket(pageSize, pageIndex, status);
+            return Ok(listPurchaseTicket);
+        }
+
+        [HttpGet("/getListPurchaseTicketByUserId/{pageSize}/{pageIndex}/{status}")]
+        public async Task<IActionResult> GetListPurchaseTicketByUserId(int pageSize, int pageIndex, PurchaseTicketStatus status)
+        {
+            var listPurchaseTicket = await _purchaseTicketService.GetListPurchaseTicketByUserId(pageSize, pageIndex, status);
+            return Ok(listPurchaseTicket);
+        }
+
+        [HttpGet("/getPurchaseTicketDetail/{purchaseTicketId}")]
+        public async Task<IActionResult> GetPurchaseTicketDetail(Guid purchaseTicketId)
+        {
+            var listPurchaseTicket = await _purchaseTicketService.GetPurchaseTicketDetail(purchaseTicketId);
             return Ok(listPurchaseTicket);
         }
     }
