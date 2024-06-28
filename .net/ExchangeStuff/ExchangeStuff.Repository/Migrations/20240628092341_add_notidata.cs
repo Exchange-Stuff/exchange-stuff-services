65262d704ed80b702324129be12c247ac91dfda0
Nguyen Minh Vu <115516869+nguynminhvu@users.noreply.github.com>
2024-06-28 16:24:06 +0700

    data

---
+++ .net/ExchangeStuff/ExchangeStuff.Repository/Migrations/20240628092341_add_notidata.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExchangeStuff.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_notidata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notification_Accounts_AccountId",
+                table: "Notification");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notification",
+                table: "Notification");
+
+            migrationBuilder.RenameTable(
+                name: "Notification",
+                newName: "Notifications");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notification_AccountId",
+                table: "Notifications",
+                newName: "IX_Notifications_AccountId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notifications",
+                table: "Notifications",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Accounts_AccountId",
+                table: "Notifications",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Accounts_AccountId",
+                table: "Notifications");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notifications",
+                table: "Notifications");
+
+            migrationBuilder.RenameTable(
+                name: "Notifications",
+                newName: "Notification");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notifications_AccountId",
+                table: "Notification",
+                newName: "IX_Notification_AccountId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notification",
+                table: "Notification",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notification_Accounts_AccountId",
+                table: "Notification",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
