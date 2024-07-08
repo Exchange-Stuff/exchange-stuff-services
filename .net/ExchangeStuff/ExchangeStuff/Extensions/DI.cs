e6ce75de1018d1e1fb1f75712818b6b9d0767632
Nguyen Minh Vu <115516869+nguynminhvu@users.noreply.github.com>
2024-07-08 20:13:53 +0700

    update db

--- .net/ExchangeStuff/ExchangeStuff/Extensions/DI.cs
+++ .net/ExchangeStuff/ExchangeStuff/Extensions/DI.cs
@@ -2,7 +2,9 @@
 using ExchangeStuff.AuthOptions.Jwts;
 using ExchangeStuff.AuthOptions.Requirements;
 using ExchangeStuff.AuthOptions.SwaggerOptions;
+using ExchangeStuff.Repository.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExchangeStuff.Extensions
 {
@@ -15,8 +17,17 @@ namespace ExchangeStuff.Extensions
             services.AddESJwt(configuration);
             services.AddTransient<IAuthorizationPolicyProvider, ESPolicy>();
             services.AddTransient<IAuthorizationHandler, ActionRequirementHandler>();
+            services.AddDbContext<ExchangeStuffContext>(x => x.UseSqlServer(GetConnectionString()));
+            services.AddScoped<Func<ExchangeStuffContext>>(x => () => x.GetService<ExchangeStuffContext>()!);
         }
-
+        private static string GetConnectionString()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                  .SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json", true, true)
+                  .Build();
+            return config.GetConnectionString("ExchangeStuffDb")!;
+        }
         public static void UseMiddleware(this IApplicationBuilder app)
         {
             app.UseMiddleware<JwtOption>();
