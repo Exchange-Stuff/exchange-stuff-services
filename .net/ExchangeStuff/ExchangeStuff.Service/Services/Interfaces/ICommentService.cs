9a81d9d63f31c47ba61d776a2eb64c1aaaa415ed
h0-hi3u <hieuenglishabc@gmail.com>
2024-07-18 11:29:01 +0700

    add paging

--- .net/ExchangeStuff/ExchangeStuff.Service/Services/Interfaces/ICommentService.cs
+++ .net/ExchangeStuff/ExchangeStuff.Service/Services/Interfaces/ICommentService.cs
@@ -5,7 +5,7 @@ namespace ExchangeStuff.Service.Services.Interfaces;
 
 public interface ICommentService
 {
-    Task<List<CommentViewModel>> GetCommentByProductId(Guid id);
+    Task<List<CommentViewModel>> GetCommentByProductId(Guid id, int pageSize, int pageIndex);
     Task<bool> CreateComment(CreateCommentModel request);
     Task<bool> UpdateComment(UpdateCommentModel request);
     Task<int> GetTotalCount(Guid productId);
