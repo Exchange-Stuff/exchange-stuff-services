c6d41178b3023102c896a43d49f04792ad74a130
h0-hi3u <hieuenglishabc@gmail.com>
2024-07-21 21:54:10 +0700

    [Dashboard] get report purchase this week

---
+++ .net/ExchangeStuff/ExchangeStuff.Service/Services/Interfaces/IDashboardService.cs
@@ -0,0 +1,16 @@
+﻿using ExchangeStuff.Service.Models.Dashboard;
+using ExchangeStuff.Service.Models.PurchaseTicket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeStuff.Service.Services.Interfaces;
+
+public interface IDashboardService
+{
+    Task<ReportPurchaseTicket> GetReportPurchaseTicketThisWeek();
+    Task<List<PurchaseTicketViewModel>> GetListPurchaseThisWeek();
+
+}
