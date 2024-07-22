bd25f108ebadf646d820fae91147ccdbb3f2551d
h0-hi3u <hieuenglishabc@gmail.com>
2024-07-22 23:27:24 +0700

    get rating by purchase id

--- .net/ExchangeStuff/ExchangeStuff.Service/Models/Rating/RatingViewModel.cs
+++ .net/ExchangeStuff/ExchangeStuff.Service/Models/Rating/RatingViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using ExchangeStuff.Core.Common;
+using ExchangeStuff.Core.Enums;
 
 namespace ExchangeStuff.Service.Models.Rating;
 
 public class RatingViewModel : BaseEntity<Guid>
 {
+    public string Content { get; set; }
+    public EvaluateType EvaluateType { get; set; }
 }
